da05e8ae0891502b2d90a1230ccb3ac9cedf5e92
Atharv Kotekar <atharvkotekar7@gmail.com>
2025-04-17 14:52:56 +0530

    intial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Atharv on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
